203752643c2a251a8f78f8256fc855049c452866
Abbas Ishaq <37180352+aishaq-py@users.noreply.github.com>
2022-12-06 16:50:20 +0000

    Comments added to main.py

--- higher_lower_task/main.py
+++ higher_lower_task/main.py
@@ -2,22 +2,35 @@ from random import randint
 
 numb = randint(1, 13)
 
+#main function
 for i in range(10):
+  
+  #Print starting number - only occurs before first guess
   if i == 0:
     print("Starting number: " + str(numb))
 
+  #user input to guess H or L - converts to lower
   guess = input(
     "Input \"H\" if the next number is higher, or \"L\" if the next number is lower.\n"
   )
   guess = guess.lower()
+  
+  #assigns previous number to previous
   previous = numb
   numb = randint(1, 13)
+  
+  #rerolls numb until numb does not equal previous
   while previous == numb:
     numb = randint(1, 13)
   print("Next number: " + str(numb))
+  
+  #exit condition after 10th run
   if i == 9:
     print("You win")
     break
+    
+  #comparator for previous to current numb, followed by user's guess
+  #correct guess = continue, wrong guess = exit loop
   if previous > numb:
     if guess == "l":
       pass
