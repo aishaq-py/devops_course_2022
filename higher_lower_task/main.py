a03233a2748907e63c4c07a7964987df1b3f6126
Abbas Ishaq <37180352+aishaq-py@users.noreply.github.com>
2022-12-09 11:26:19 +0000

    Updated some inefficiences
    1) Collated range(10) and int == 9 into the variable "iterations" so that modifying "iterations" will automatically change both values.
    
    2) Moved print("Starting number...") out of the for loop so that the loop does not have to check for i == 0 every loop.

--- higher_lower_task/main.py
+++ higher_lower_task/main.py
@@ -1,14 +1,13 @@
 from random import randint
 
 numb = randint(1, 13)
+iterations = 10
+print("Starting number: " + str(numb)) #Print starting number - only occurs before first guess
+
 
 #main function
-for i in range(10):
+for i in range(iterations):
   
-  #Print starting number - only occurs before first guess
-  if i == 0:
-    print("Starting number: " + str(numb))
-
   #user input to guess H or L - converts to lower
   guess = input(
     "Input \"H\" if the next number is higher, or \"L\" if the next number is lower.\n"
@@ -26,8 +25,8 @@ for i in range(10):
     numb = randint(1, 13)
   print("Next number: " + str(numb))
   
-  #exit condition after 10th run
-  if i == 9:
+  #exit condition after Nth run
+  if i == int(iterations)-1:
     print("You win")
     break
     
